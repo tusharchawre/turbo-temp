0af3312661ce8fcaff342b1220c84850738519c2
Tushar Chawre <tusharchawre0240@gmail.com>
2025-01-29 21:36:18 +0530

    Content layer exp

--- apps/web/components/docs/mdx-components.tsx
+++ apps/web/components/docs/mdx-components.tsx
@@ -1,9 +1,14 @@
 import { useMDXComponent } from "next-contentlayer2/hooks"
+import { Navbar } from "../landing-page/Navbar"
 
 
 
 interface MdxProps {
     code: string
+}
+
+const components = {
+    Navbar
 }
 
 
@@ -12,6 +17,6 @@ export function Mdx ({code}: MdxProps){
     const Component = useMDXComponent(code)
 
     return (
-        <Component />
+        <Component components={components} />
     )
 }
