b71b935d2e08bc152fdb0691908b334043405407
Tushar Chawre <tusharchawre0240@gmail.com>
2025-01-31 12:14:19 +0530

    Test deploy

--- apps/web/app/docs/[[...slug]]/page.tsx
+++ apps/web/app/docs/[[...slug]]/page.tsx
@@ -4,14 +4,12 @@ import { useLiveReload } from "next-contentlayer2/hooks"
 import { notFound } from "next/navigation"
 
 interface DocsPageProps{
-    params: {
-        slug: string[]
-    }
+    params : Promise<{slug: string[]}>
 }
 
 
 async function getDocFromParmas({params} : DocsPageProps){
-    const slug = params.slug?.join("/") || ""
+    const slug = (await params).slug?.join("/") || ""
 
     const doc = allDocs.find((doc)=> doc.slugAsParams === slug)
 
